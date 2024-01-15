9b1add0f464bba532443c5daeb97d671a6cfc563
simran417 <simranvohra417@email.com>
2024-01-15 13:17:25 +0530

    initial commi

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a=int(input("enter first no.: "))
+operator=input("enter operator(+,-,*,/)")
+b=int(input("enter second no.: "))
+if operator=="+":
+    print(a+b)
+if operator=="-":
+    print(a-b)
+if operator=="*":
+    print(a*b)    
+if operator=="/":
+    print(a/b)    
