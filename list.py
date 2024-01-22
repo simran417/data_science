6031378a579436ec901d65f594380eee64e62faf
simran417 <simranvohra417@email.com>
2024-01-22 19:32:02 +0530

    lists

---
+++ list.py
@@ -0,0 +1,20 @@
+#lists:
+'''a=[1,2,3,4,5]
+print(a)
+print(a[3])
+b=['a',8,True,"simran"]
+print(b)'''
+
+#slicing in lists:
+'''friends=["sneha","kajal","simran","shruti","shivangi"]
+print(friends[0:3])'''
+
+#operations on list:
+a=[1,4,9,2,5,3]
+#a.sort()
+##a.reverse()
+#a.append(6)
+#a.pop(4)
+#a.insert(2,7)
+a.remove(5)
+print(a)
