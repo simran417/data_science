e5df8a5ec3356d62a5c887b31bbf449a9fb81d05
simran417 <simranvohra417@email.com>
2024-01-29 20:04:09 +0530

    numpy_intro

---
+++ numpy_intro.py
@@ -0,0 +1,18 @@
+#difference b/w array and list:
+#array:
+'''import numpy as np 
+x = np.array([1,2,3,4])
+print(x)
+print(type(x))'''
+'''
+#list:
+y = [1.2,3,4]
+print(y)
+print(type(y))'''
+
+#to check speed: use %timeit function
+import timeit
+#in list:
+#%timeit [j**4 for j in range(1,9)]
+          
+
